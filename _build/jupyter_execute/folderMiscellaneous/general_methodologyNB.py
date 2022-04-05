f7d5163fb3757504465ba3bf63b602c65bff14d9
csxADS <csxPFE@gmail.com>
2022-04-05 15:10:49 +0200

    general metodo NB

---
+++ _build/jupyter_execute/folderMiscellaneous/general_methodologyNB.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # General Methodology
+# 
+# ## Reliability prediction for miscellaneous items
+# 
+# For the four different sections (System, EEE, Mechanical and Miscellaneous), the same principle will be applied with for each section a clear definition of the objective(s) and then a clear explanation of the choices. When relevant, examples will be used to illustrate.
+# 
+# The level of recommendation/rule or permission will be clearly indicated so that it is easy for the user to adapt its application of the method based on its needs.
+# 
+# This chapter deals with the miscellaneous items which are represented by all parts/item (or sets of parts) which are not fully electrical parts, nor fully mechanical parts nor fully structural parts. The current reliability models (EEE parts, mechanical parts) are not adapted and are not the preferred way to support reliability predictions for these items. Therefore, other ways to provide reliability estimates are proposed in this chapter and are detailed in the next sections.
+# 
+# The [following figure](figure4_1) represents examples of some miscellaneous parts/items:
+# 
+# ![alt text](picture/figure4_1.png "Examples of some miscellaneous items")
+# 
+# The list provided below may contain some items that are in fact EEE or mechanical type items, but which are classified as “miscellaneous” because the modelling approach for EEE/mechanical is not considered as completely suitable.
+# 
+# One of the major characteristics of miscellaneous parts/items is that they are various (functions, technologies, nature) and very specific in the sense they are designed to support dedicated and usual functions (e.g. battery) in a satellite:
+# 
+# * They cover a wide range of Technologies (e.g. Chemical, fluidic…) apart “simple” electrical or mechanical technologies,
+# * They are not fully covered by elementary « EEE or Mechanical » models (e.g. a battery cathode is not a pure EEE part),
+# * The failures may originate from particular failure mechanisms (e.g. catalytic bed degradation for N2H4 thrusters),
+# * They may be supported by « Innovative » technology (e.g. Plasmic propulsion) with reduced in-orbit return,
+# * They can be submitted to Wear-out process (e.g. battery calendar aging) depending on their nature.
+# 
+
+# <div class="admonition danger">
+# <p class="admonition-title">Don't try this at home</p>
+# <p>...</p>
+# </div>
+
+# ```{note}
+# Here is a note!
+# ```
+
+# In[ ]:
+
+
+
+
